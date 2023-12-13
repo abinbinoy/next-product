c14f5b334439907da6350345a34f601f48dd772c
Abin Binoy <abinmalayil524@gmail.com>
2023-12-13 15:32:24 +0530

    fix: image url

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    images: {
+      domains: ['i.dummyjson.com'],
+    },
+  }
+  
+  module.exports = nextConfig
